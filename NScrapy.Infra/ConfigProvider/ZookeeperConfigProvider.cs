2703e6d174cc4fdd9c18fd966590e107089db20d
Elendil <zy4182@sina.com>
2018-07-09 23:42:03 +0800

    refactor zookeeper config provider Also use a more elegent way to exit in memory Nscrapy Crawl mode

--- NScrapy.Infra/ConfigProvider/ZookeeperConfigProvider.cs
+++ NScrapy.Infra/ConfigProvider/ZookeeperConfigProvider.cs
@@ -71,27 +71,32 @@ namespace NScrapy.Infra.ConfigProvider
             zkEndpoint = localConfig["AppSettings:ZookeeperEndpoint"];
         }
 
-        public static void Create(string path,string data)
+        public static void Create(string path,string data, CreateMode createMode)
         {
-            var bytes = data!=null? Encoding.UTF8.GetBytes(data):null;
-            var subPathes = path.Split("/",StringSplitOptions.RemoveEmptyEntries).ToList();
+            var bytes = data != null ? Encoding.UTF8.GetBytes(data) : null;
+            var subPathes = path.Split("/", StringSplitOptions.RemoveEmptyEntries).ToList();
             if (subPathes.Count > 1)
             {
-                Create(string.Join("/", subPathes.Take(subPathes.Count - 1)), string.Empty);
+                Create(string.Join("/", subPathes.Take(subPathes.Count - 1)), string.Empty, createMode);
             }
             //Since each path node need to be created by their sequence in the path, we should not run the task in async way, 
             //have to wait until the create is done for current path node 
-            ZooKeeper.Using(zkEndpoint, 1000, new NScrapyConfigWatcher(),async zk =>
+            ZooKeeper.Using(zkEndpoint, 1000, new NScrapyConfigWatcher(), async zk =>
             {
-                if(path[0]!='/')
+                if (path[0] != '/')
                 {
                     path = "/" + path;
                 }
-                if (await zk.existsAsync(path)==null)
+                if (await zk.existsAsync(path) == null)
                 {
-                    await zk.createAsync(path, bytes, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                    await zk.createAsync(path, bytes, Ids.OPEN_ACL_UNSAFE, createMode);
                 }
             }).Wait();
+        }
+
+        public static void Create(string path,string data)
+        {
+            Create(path, data, CreateMode.PERSISTENT);
         }
 
         public async static Task<string> GetAsync(string path)
